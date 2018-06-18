80df0414345cdff423aa679c8c5fd9b76b53c596
Michael Knudsen <micknudsen@gmail.com>
2018-06-18 08:54:24 +0200

    Configured guessadapt console script entry point

--- setup.py
+++ setup.py
@@ -7,6 +7,10 @@ setup(
     packages=find_packages('src'),
     package_dir={'': 'src'},
 
+    entry_points={
+        'console_scripts': ['guessadapt = guessadapt:main']
+    },
+
     author='Michael Knudsen',
     author_email='michaelk@clin.au.dk',
     license='MIT'
