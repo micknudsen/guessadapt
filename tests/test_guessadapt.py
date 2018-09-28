656204d5d6c28df46e3aa64e688bed4f42e49439
Michael Knudsen <micknudsen@gmail.com>
2018-09-28 09:49:13 +0200

    Added unit test for count_adapters method

--- tests/test_guessadapt.py
+++ tests/test_guessadapt.py
@@ -1,7 +1,19 @@
 import unittest
 
+from guessadapt.client import count_adapters
+
 
 class TestGuessAdapt(unittest.TestCase):
 
-    def test_true(self):
-        self.assertTrue(True)
+    def test_count_adapters(self):
+
+        default_adapters = ['AGATCGGAAGAGC' ,'TGGAATTCTCGG', 'CTGTCTCTTATA']
+
+        adapter_counts = count_adapters(fastq='tests/test.fastq.gz',
+                                        adapters=default_adapters)
+
+        self.assertEqual(len(adapter_counts), 3)
+
+        self.assertEqual(adapter_counts['AGATCGGAAGAGC'], 3)
+        self.assertEqual(adapter_counts['TGGAATTCTCGG'], 2)
+        self.assertEqual(adapter_counts['CTGTCTCTTATA'], 1)
