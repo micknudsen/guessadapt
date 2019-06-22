a7b9a82fac048dfc2fccdff9f18a6211126008a6
Michael Knudsen <micknudsen@gmail.com>
2019-06-22 07:11:52 -0700

    Test counter_adapters with sequence limit

--- tests/test_guessadapt.py
+++ tests/test_guessadapt.py
@@ -18,3 +18,16 @@ class TestGuessAdapt(unittest.TestCase):
         self.assertEqual(adapter_counts['AGATCGGAAGAGC'], 3)
         self.assertEqual(adapter_counts['TGGAATTCTCGG'], 2)
         self.assertEqual(adapter_counts['CTGTCTCTTATA'], 1)
+
+    def test_count_adapters_with_limit(self):
+
+        default_adapters = ['AGATCGGAAGAGC', 'TGGAATTCTCGG', 'CTGTCTCTTATA']
+
+        with open('tests/test.fastq', 'r') as handle:
+            adapter_counts = count_adapters(handle=handle,
+                                            adapters=default_adapters,
+                                            limit=3)
+
+        self.assertEqual(len(adapter_counts), 1)
+
+        self.assertEqual(adapter_counts['AGATCGGAAGAGC'], 3)
