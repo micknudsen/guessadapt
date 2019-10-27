72ecc56ffff0f7ca1af4d438d8d6aac5fac21f65
Michael Knudsen <micknudsen@gmail.com>
2019-10-27 08:09:02 +0100

    Added test for iterquads function

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,9 @@
 import unittest
 
+from guessadapt.utils import iterquads
+
 
 class TestUtils(unittest.TestCase):
-    pass
+
+    def test_iterquads(self):
+        self.assertEqual(list(iterquads(list('ABCDEFGH'))), [list('ABCD'), list('EFGH')])
