2087a414ace20f7ca895f2e407ce0d55ed0498b5
Michael Knudsen <micknudsen@gmail.com>
2019-10-27 08:34:59 +0100

    Implemented iterquads function

--- src/guessadapt/utils.py
+++ src/guessadapt/utils.py
@@ -1,5 +1,11 @@
-from typing import Iterator
+from typing import Iterator, Tuple
 
 
-def iterquads(itr: Iterator[str]) -> Iterator[str]:
-    pass
+def iterquads(itr: Iterator[str]) -> Iterator[Tuple[str, str, str, str]]:
+    i = 0
+    while True:
+        try:
+            yield next(itr), next(itr), next(itr), next(itr)
+            i += 4
+        except StopIteration:
+            break
